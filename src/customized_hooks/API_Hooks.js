6075963b69d7c4a2cb19a716303cc57e33445fd1
seba-salahat5 <80277805+seba-salahat5@users.noreply.github.com>
2024-01-01 01:23:03 +0200

    Axios API Manipulations, API custom hook

---
+++ src/customized_hooks/API_Hooks.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export function useApi(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function loadData() {
+      setLoading(true);
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadData();
+  }, [url]);
+
+  return {
+    data,
+    loading,
+    error,
+  };
+}
--- src/pages/HomeContainer.js
+++ src/pages/HomeContainer.js
@@ -10,6 +10,8 @@ import CardsGridComponent from '../home_components/CardsGridComponent.js';
 import LoadingSpinner from '../shared_components/LoadingSpinner.js';
 import FooterComponent from '../layouts/FooterComponent.js';
 
+import { useApi } from '../customized_hooks/API_Hooks.js';
+
 const StyledMain = styled.main`
 margin-top: 15px;
 margin-bottom: 15px;
@@ -50,42 +52,16 @@ text-decoration: none;
 color: inherit;
 `;
 export default function HomeContainer() {
-    const [topicsArray, setTopicsArray] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [topicsArray, setTopicsArray] = useState(null);
     const sortOptions = ['Default', 'Topic Title', 'Author Name'];
     const FilterOptions = ['Default', 'Web Development Languages', 'Frontend Frameworks and Libraries', 'Backend Frameworks and Libraries', 'Databases and APIs', 'Web Development Concepts and Technologies'];
 
-    const fetchTopics = async (searchInput) => {
-        const apiUrl = 'https://tap-web-1.herokuapp.com/topics';
-
-        setLoading(true);
-        let url = new URL(`${apiUrl}/list`);
-
-        if (searchInput) {
-            url.searchParams.set('phrase', searchInput);
-        }
-        try {
-            let response = await fetch(url);
-            switch (response.status) {
-                case 200:
-                    const data = await response.json();
-                    setTopicsArray(data);
-                    setLoading(false);
-                    break;
-                default:
-                    setLoading(false);
-                    return null;
-            }
-        } catch (error) {
-            console.log(error);
-            setLoading(false);
-            return null;
-        }
-    };
+    const apiUrl = 'https://tap-web-1.herokuapp.com/topics';
+    const { data, loading, error } = useApi(`${apiUrl}/list`);
 
     useEffect(() => {
-        fetchTopics();
-    }, []);
+        setTopicsArray(data);
+    }, [data]);
 
     return (
         <React.Fragment>
@@ -103,24 +79,35 @@ export default function HomeContainer() {
                     <LoadingSpinner />
                 ) : (
                     <>
-                        {topicsArray == null ? (
+                        {error ? (
                             <div>
                                 <h3><strong>Something went wrong. Web topics failed to load.</strong></h3>
                             </div>
                         ) : (
-                            <div>
-                                <h3><strong>"{topicsArray.length}" Web Topics Found</strong></h3>
-                            </div>
+                            <>
+                                {topicsArray !== null ? (
+                                    <>
+                                        <div>
+                                            <h3><strong>"{data.length}" Web Topics Found</strong></h3>
+                                        </div>
+                                        <CardsGridComponent>
+                                            {data.map((topic) => (
+                                                <StyledLink to={`/details/${topic.id}?topicId=${topic.id}`} key={topic.id}>
+                                                    <Column key={topic.id}>
+                                                        <CardComponent image={topic.image} topic={topic.topic} category={topic.category} rating={topic.rating} name={topic.name} />
+                                                    </Column>
+                                                </StyledLink>
+                                            ))}
+                                        </CardsGridComponent>
+                                    </>
+
+                                ) : (
+                                    <div>
+                                        <h3><strong>Something went wrong. Web topics failed to load.</strong></h3>
+                                    </div>
+                                )}
+                            </>
                         )}
-                        <CardsGridComponent>
-                            {topicsArray.map((topic) => (
-                                <StyledLink to={`/details/${topic.id}?topicId=${topic.id}`} key={topic.id}>
-                                    <Column key={topic.id}>
-                                        <CardComponent image={topic.image} topic={topic.topic} category={topic.category} rating={topic.rating} name={topic.name} />
-                                    </Column>
-                                </StyledLink>
-                            ))}
-                        </CardsGridComponent>
                     </>
                 )}
 
