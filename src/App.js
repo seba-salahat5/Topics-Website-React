eaea2a481e31b048bed432475e215dce47149a22
seba-salahat5 <80277805+seba-salahat5@users.noreply.github.com>
2024-01-04 10:56:43 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <ThemeProvider>
       <FavouritesProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/topics-react">
           <Layout>
             <Switch>
               <Route exact path="/" component={HomeContainer} />
